18f80d6809b99ac14b9a5b34fb1c2fb847f49bee
AnjirHossain <anjirh@uw.edu>
2017-11-21 11:49:03 -0800

    Reading from firebase

--- horizanwebapp/src/index.js
+++ horizanwebapp/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from './firebase';
+
+// r:database
+let database = firebase.database();
+let usersRef = database.ref().child('users');
+
+usersRef.on('value', snapshot => {
+	console.log('Users: ', snapshot.val());
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
