db728fc929a05fa8e2e36cbddb7b64e1de7fb4cd
AnjirHossain <anjirh@uw.edu>
2017-11-21 15:04:56 -0800

    Started modal for authentication

--- horizanwebapp/src/App.js
+++ horizanwebapp/src/App.js
@@ -2,17 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Modal from 'react-modal';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authModalIsOpen: false
+    };
+  }
+  openAuthModal() {
+    this.setState({
+      authModalIsOpen: true
+    })
+  }
+  closeAuthModal() {
+    this.setState({
+      authModalIsOpen: false
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <Modal
+          isOpen={this.state.authModalIsOpen}
+          onRequestClose={this.closeAuthModal.bind(this)}
+          contentLabel="Modal">
+          <h1>Login/Signup</h1>
+          <button onClick={this.closeAuthModal.bind(this)}>
+            Close Modal
+          </button>
+        </Modal>
+        <header>
+          <nav className="Navbar">
+            <div className="Logo">
+              Horizan
+            </div>
+            <div className="Links">
+              <ul>
+                <li>
+                  <button onClick={this.openAuthModal.bind(this)}>
+                    Signup
+                  </button>
+                </li>
+                <li>
+                  <button onClick={this.openAuthModal.bind(this)}>
+                    Login
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
