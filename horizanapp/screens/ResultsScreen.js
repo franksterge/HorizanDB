70ef5c11e3cdfb4454299481758db5b6ff66ef60
dingdalton <daltonhtx@gmail.com>
2018-08-16 02:40:14 -0500

    stuff added

--- horizanapp/screens/ResultsScreen.js
+++ horizanapp/screens/ResultsScreen.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import styles from ".././assets/styles/styles";
 
 export default class ResultsScreen extends React.Component {
+
+  static navigationOptions = {
+    tabBarLabel: '',
+    headerStyle: {
+        backgroundColor: 'white',
+        elevation: 0,
+        shadowOpacity: 0,
+      }
+  };
+
   render() {
+    const results = this.props.navigation.getParam('results', 'No results, some error occured :(');
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container} >
+        <Text style={styles.text}>
+          Your Matches
+        </Text>
+          <FlatList
+          data={JSON.parse(results)}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item}) =>
+          <View style={styles.flatview}>
+            <Text style={styles.para}>{item}</Text>
+            <Text style={styles.para}>{item}</Text>
+          </View>
+          }
+          keyExtractor={item => item}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
